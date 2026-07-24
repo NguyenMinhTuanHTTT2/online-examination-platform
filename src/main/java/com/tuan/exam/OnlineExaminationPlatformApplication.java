65d85f5185ef80687b5ab191f3bfbdeb21784eb9
NguyenMinhTuanHTTT2 <chanhtungtuan2@gmail.com>
2026-07-24 20:45:04 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/tuan/exam/OnlineExaminationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tuan.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExaminationPlatformApplication.class, args);
+	}
+
+}
